8eb0d536c3e14cf32aea19f9de26dd86bf5af3cc
Mark Campanelli <campy@instreamdesigns.com>
2011-09-24 21:31:49 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VaccineManager::Application.config.secret_token = '59684a04775fedbf52317e10cc4400bc8e3a86ded4ac0eadc3858715304532affb910f6fbded234ec4ffb5b1914c5bc75f97e7d73a7714271a12a0ea51005646'
